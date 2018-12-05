699fc448f9e31ca0217edd457111ebd6d87c7d11
Александр <aparamonov@iMac-Aleksandr.local>
2018-12-05 18:06:02 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+    "context"
+    "encoding/json"
+    "errors"
+    "flag"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+    "github.com/prometheus/common/version"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+    "os/signal"
+    "strconv"
+    "syscall"
+    "time"
+)
+
+var addr = flag.String("listen-address", "0.0.0.0:9601", "The address to listen on for HTTP requests.")
+var interval = flag.Int("interval", 3600, "Interval (in seconds) for querying balance.")
+
+var balanceGauge *prometheus.GaugeVec
+
+type smscResponse struct {
+    Balance string `json:"balance"`
+}
+
+type smscConfig struct {
+    Login    string
+    Password string
+}
+
+var credentials = smscConfig{}
+
+func init() {
+    balanceGauge = prometheus.NewGaugeVec(
+        prometheus.GaugeOpts{
+            Subsystem: "balance",
+            Name:      "smsc",
+            Help:      "Balance for service in smsc account",
+        },
+        []string{"service"},
+    )
+
+    prometheus.MustRegister(balanceGauge)
+}
+
+func main() {
+    log.Println("Starting Smsc balance exporter", version.Info())
+    log.Println("Build context", version.BuildContext())
+
+    if err := readConfig(); err != nil {
+        log.Println(err.Error())
+        return
+    }
+
+    loadBalance()
+    go startBalanceUpdater(*interval)
+    srv := &http.Server{
+        Addr:         *addr,
+        WriteTimeout: time.Second * 2,
+        ReadTimeout:  time.Second * 2,
+        IdleTimeout:  time.Second * 60,
+
+        Handler: nil,
+    }
+
+    http.Handle("/metrics", promhttp.Handler())
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "static/index.html")
+    })
+
+    go func() {
+        log.Fatal(srv.ListenAndServe())
+    }()
+
+    log.Printf("Smsc balance exporter has been started at address %s", *addr)
+    log.Printf("Exporter will update balance every %d seconds", *interval)
+
+    c := make(chan os.Signal, 1)
+
+    signal.Notify(c, os.Interrupt)
+    signal.Notify(c, syscall.SIGTERM)
+
+    <-c
+
+    log.Println("Smsc balance exporter shutdown")
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+    defer cancel()
+
+    err := srv.Shutdown(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    os.Exit(0)
+}
+
+func readConfig() error {
+    if login, ok := os.LookupEnv("SMSC_LOGIN"); ok {
+        credentials.Login = login
+    } else {
+        return errors.New("environment \"SMSC_LOGIN\" is not set")
+    }
+
+    if password, ok := os.LookupEnv("SMSC_PASSWORD"); ok {
+        credentials.Password = password
+    } else {
+        return errors.New("environment \"SMSC_PASSWORD\" is not set")
+    }
+
+    return nil
+}
+
+func startBalanceUpdater(i int) {
+    for {
+        time.Sleep(time.Second * time.Duration(i))
+        loadBalance()
+    }
+}
+
+func loadBalance() {
+    body, err := loadBody()
+    if err != nil {
+        log.Printf("Error fetching balance: %s", err.Error())
+    }
+
+    jsonResponse := smscResponse{}
+    if err := json.Unmarshal(body, &jsonResponse); err != nil {
+        log.Printf("Error fetching balance: %s", err.Error())
+    }
+    if b, err := strconv.ParseFloat(jsonResponse.Balance, 2); err != nil {
+        log.Printf("Cannot parse balance: %s", err.Error())
+    } else {
+        balanceGauge.With(prometheus.Labels{"service": credentials.Login}).Set(b)
+    }
+}
+
+func loadBody() ([]byte, error) {
+    client := http.Client{
+        Timeout: time.Second * 2,
+    }
+
+    req, err := http.NewRequest(http.MethodGet, "https://smsc.ru/sys/balance.php", nil)
+    q := req.URL.Query()
+    q.Add("login", credentials.Login)
+    q.Add("psw", credentials.Password)
+    q.Add("fmt", "3")
+    req.URL.RawQuery = q.Encode()
+
+    if err != nil {
+        log.Printf("Request error: %s", err.Error())
+        return []byte{}, err
+    }
+
+    res, err := client.Do(req)
+    if err != nil {
+        log.Printf("Request error: %s", err.Error())
+        return []byte{}, err
+    }
+
+    defer func() {
+        err := res.Body.Close()
+        if err != nil {
+            log.Printf("Error close response body: %s", err.Error())
+        }
+    }()
+
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        log.Printf("Error read response: %s", err.Error())
+    }
+
+    return body, nil
+}
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Smsc balance exporter
+
+The smsc balance exporter allows exporting balance for [smsc gateway](https://smsc.ru)
+
+## How it works
+Exporter querying balance every hour (by default) and save it value in memory
+
+## Configuration
+You must set environment variables:
+
+* `SMSC_LOGIN` - your login
+* `SMSC_PASSWORD` - your password
+
+## Command-line flags
+
+* `listen-address` - The address to listen on for HTTP requests. (Default: `0.0.0.0:9601`)
+* `interval` - The address to listen on for HTTP requests. (Default: `0.0.0.0:9601`)
+
+## Running with systemctl
+
+```sh
+# Set variables you need
+SMSC_EXPORTER_VERSION=v0.1.0-alpha
+SMSC_EXPORTER_PLATFORM=linux
+SMSC_EXPORTER_ARCH=amd64
+
+# Download release
+wget https://github.com/xxxcoltxxx/smsc-balance-exporter/releases/download/${SMSC_EXPORTER_VERSION}/smsc_balance_exporter_${SMSC_EXPORTER_VERSION}_${SMSC_EXPORTER_PLATFORM}_${SMSC_EXPORTER_ARCH}.tar.gz
+tar xvzf smsc_balance_exporter_${SMSC_EXPORTER_VERSION}_${SMSC_EXPORTER_PLATFORM}_${SMSC_EXPORTER_ARCH}.tar.gz
+mv ./smsc_balance_exporter_${SMSC_EXPORTER_VERSION}_${SMSC_EXPORTER_PLATFORM}_${SMSC_EXPORTER_ARCH} /usr/local/bin/smsc_balance_exporter
+
+# Add service to systemctl
+cat <<EOF >> /etc/systemd/system/smsc_balance_exporter.service
+[Unit]
+Description=Smsc Balance Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Environment="SMSC_LOGIN=<your_login> SMSC_PASSWORD=<your_password>"
+User=balance_exporter
+Group=balance_exporter
+Type=simple
+ExecStart=/usr/local/bin/smsc_balance_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemctl configuration and run service
+systemctl daemon-reload
+systemctl start smsc_balance_exporter
+systemctl status smsc_balance_exporter
+```
